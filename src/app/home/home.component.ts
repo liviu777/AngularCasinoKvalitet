c54fdf2223e28e29c0d81fe9d2cad026ed7abf06
liviulenter <lenterliviu@gmail.com>
2021-04-29 11:27:48 +0200

    Add Home adn About Components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  users = [];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+
+    console.log('get users');
+    this.apiService.get().subscribe((data: any[]) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
+
+}
